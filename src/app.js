e72526cd93151b69ada2899b6add007193f6eb7e
Karthik Reddy <karthik.april11@gmail.com>
2025-05-22 21:05:54 -0400

    update

--- src/app.js
+++ src/app.js
@@ -6,5 +6,5 @@ app.use((req,res)=>{
     res.send('Hello from server');
 })
 app.listen(3000,() => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3001');
 });
